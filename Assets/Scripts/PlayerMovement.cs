b160ab82a37badec0e3b0f8e21d339a82bf74079
superdoublekick <114491309+superdoublekick@users.noreply.github.com>
2023-02-25 13:43:33 +0200

    Player Move Forward, Model Scale

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private int movementSpeed;
+
+    private Rigidbody rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.W))
+            rb.AddForce(transform.forward * movementSpeed);
+            
     }
 }
