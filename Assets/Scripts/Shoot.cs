e14ff6c98f8f2d8985440c09c53053be328034aa
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2023-02-25 16:51:55 +0200

    Shoot: Rework
    Signed-off-by: Ivaylo Ivanov <ivaylolivanov95@gmail.com>

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -4,21 +4,13 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-   [SerializeField] private Transform firePoint;
-   public GameObject bulletPrefab;
-   public float bulletSpeed = 10;
+    [SerializeField] private Transform firePoint;
 
-    
+    [SerializeField] private GameObject bulletPrefab;
 
-    
     void Update()
     {
         if(Input.GetKey(KeyCode.Mouse0))
-        {
-            var bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-            bullet.GetComponent<Rigidbody>().velocity = firePoint.forward * bulletSpeed;
-
-        }
-
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
 }
